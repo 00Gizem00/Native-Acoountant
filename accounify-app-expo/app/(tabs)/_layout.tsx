3f0d73fb4a2cd577e300d95bde3ce7c53f5ddde1
00Gizem00 <gizem_fbl@windowslive.com>
2024-11-19 03:41:51 +0300

    Home Capture Settings tabs added

--- accounify-app-expo/app/(tabs)/_layout.tsx
+++ accounify-app-expo/app/(tabs)/_layout.tsx
@@ -36,7 +36,14 @@ export default function TabLayout() {
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
+          title: 'Capture',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="plus.bubble.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
